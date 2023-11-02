a350fb9eac78f045e548b473a94ae8f097d9bb81
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-02 22:00:05 +0300

    Initial commit

---
+++ Library.cs
@@ -0,0 +1,137 @@
+﻿using ClassLibrary;
+
+namespace Library
+{
+    internal class Library
+    {
+        private List<ILoanable> _books = new List<ILoanable>();
+        private List<ILoanable> _dvds = new List<ILoanable>();
+        private List<ILoanable> _cdds = new List<ILoanable>();
+
+        public Library()
+        {
+            _books = BookInitialize();
+        }
+
+        public void BorrowMenu()
+        {
+           
+
+            int userInput = -1;
+
+            while (userInput != 0)
+            {
+                Console.WriteLine("Что бы вы хотели взять?: ");
+                Console.WriteLine("1. Книгу");
+                Console.WriteLine("2. DVD");
+                Console.WriteLine("3. CD");
+                Console.WriteLine("0. Выйти");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        BorrowItem(_books);
+                        return;
+
+                    case "2":
+                        BorrowItem(_dvds);
+                        return;
+
+                    case "3":
+                        BorrowItem(_cdds);
+                        return;
+
+                    case "4":
+                        userInput = 0;
+                        return;
+                }
+            }
+        }
+
+        private void BorrowItem(List<ILoanable> printables)
+        {
+            Console.WriteLine("Список доступных книг:");
+
+            foreach (var item in printables)
+            {
+                if (item.IsLoanable)
+                {
+                    IPrintable printable = (IPrintable)item;
+                    printable.Print();
+                }
+            }
+
+            Console.WriteLine("Введите ID книги, которую вы хотите взять (или 0 для отмены):");
+            int idToBorrow = int.Parse(Console.ReadLine());
+
+            if (idToBorrow == 0)
+            {
+                Console.WriteLine("Операция отменена.");
+                return;
+            }
+
+            Console.WriteLine("Введите ваше имя для бронирования:");
+            string borrowerName = Console.ReadLine();
+
+            foreach (var item in printables)
+            {
+                if (item.IsLoanable && item is Book book && book.ID == idToBorrow)
+                {
+                    
+                    IPrintable printable = (IPrintable)book;
+                    printable.Print();
+                    book.Borrow(idToBorrow, borrowerName);
+                    return;
+                }
+            }
+
+            Console.WriteLine("Книга с указанным ID не найдена или недоступна для выдачи.");
+        }
+
+        public void AddItemMenu()
+        {
+            Console.WriteLine("Что вы хотите добавить? ");
+            Console.WriteLine("1. Книги");
+            Console.WriteLine("2. DVD");
+            Console.WriteLine("3. CD");
+
+            int userInput = -1;
+
+            while (userInput != 0)
+            {
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        break;
+
+                    case "4":
+                        userInput = 0;
+                        break;
+                }
+            }
+        }
+
+        public void RemoveItem()
+        {
+
+        }
+
+        public void ShowItems()
+        {
+
+        }
+
+        private List<ILoanable> BookInitialize()
+        {
+            List<ILoanable> list = new List<ILoanable>() {
+            new Book(1, "Эрих Мария Ремарк", "Черный обелиск", 11221),
+        new Book(2, "Эрих Мария Ремарк", "На Западном фронте без перемен", 15622)
+            };
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using ClassLibrary;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            int userInput = -1;
+
+            while (userInput != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Приветствуем в библиотеке!");
+                Console.WriteLine("1. Взять: ");
+                Console.WriteLine("2. Добавить: ");
+                Console.WriteLine("3. Удалить: ");
+                Console.WriteLine("4. Показать библиотеку: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        library.BorrowMenu();
+                        break;
+
+                    case "2":
+                        library.AddItemMenu();
+                        break;
+
+                    case "3":
+                        library.RemoveItem();
+                        break;
+
+                    case "4":
+                        library.ShowItems();
+                        break;
+
+                    case "5":
+                        userInput = 0;
+                        break;
+                }
+            }
+
+
+            static void ShowBooks(List<Book> books)
+            {
+                foreach (Book book in books)
+                {
+                    Console.WriteLine($"Книга {book.Title}, {book.Author}, {book.IsLoanable}");
+                }
+
+                Console.WriteLine("Какую хотите взять?");
+                string userInput = Console.ReadLine();
+
+                if (books.Where(x => x.Title == userInput).Where(x => x.IsLoanable).Any())
+                {
+                    Console.WriteLine("Успешно");
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine($"Книга {userInput} занята");
+                }
+            }
+        }
+    }
+}
